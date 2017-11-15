0c858f59499959a801bd1d213391b90a39e5a421
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2017-11-15 03:54:29 +0000

    Chuck in a concurrent worker approach for crawling

--- crawler.go
+++ crawler.go
@@ -1,7 +1,9 @@
 package gocrawler
 
 import (
+	"sort"
 	"strings"
+	"sync"
 
 	"github.com/matthewrudy/gocrawler/scraper"
 )
@@ -17,6 +19,7 @@ func (c Crawler) String() string {
 	for _, r := range c.Results {
 		strs = append(strs, r.String())
 	}
+	sort.Strings(strs)
 	return strings.Join(strs, "\n\n")
 }
 
@@ -29,10 +32,46 @@ func New(entrypoint string) Crawler {
 }
 
 func (c *Crawler) Crawl() {
+	requests := make(chan scraper.Request, 100)
+	results := make(chan scraper.Result, 100)
+
+	var wg sync.WaitGroup
+
+	for w := 1; w <= 10; w++ {
+		go worker(requests, results)
+	}
+
+	requests <- scraper.NewRequest(c.entrypoint)
+	wg.Add(1)
+
+	go manager(c, requests, results, &wg)
+
+	wg.Wait()
+}
+
+func worker(requests <-chan scraper.Request, results chan<- scraper.Result) {
 	worker := scraper.New()
+	for req := range requests {
+		results <- worker.Scrape(req)
+	}
+}
 
-	request := scraper.NewRequest(c.entrypoint)
-	result := worker.Scrape(request)
+func manager(c *Crawler, requests chan<- scraper.Request, results <-chan scraper.Result, wg *sync.WaitGroup) {
+	attempts := make(map[string]int)
 
-	c.Results[c.entrypoint] = result
+	for r := range results {
+		if r.Success {
+			// store the result
+			c.Results[r.Request.Uri] = r
+
+			for _, link := range r.Page.Links {
+				if attempts[link] < 1 {
+					attempts[link] = 1
+					requests <- scraper.NewRequest(link)
+					wg.Add(1)
+				}
+			}
+		}
+		wg.Done()
+	}
 }
